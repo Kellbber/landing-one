713ff38a8cc9a51e058de6ad21242ddf0e8d49d0
Kellbber <kellbberbarcarolo@gmail.com>
2022-10-31 18:40:16 -0300

    feat(about): implemented responsive style

--- src/components/About/style.ts
+++ src/components/About/style.ts
@@ -2,14 +2,12 @@ import styled, {css} from 'styled-components'
 import person from '../../assets/images/person-about.jpg'
 export const background = styled.section`
 ${({theme})=>css`
-
 width: 100%;
 height: 60vh;
 background-position: center;
 background-size: cover;
 background-repeat: no-repeat;
 display: flex;
-
 `}
 
 `
@@ -25,6 +23,9 @@ export const title = styled.h2`
     font-family: ${theme.constants.textTopFontFamily};
     margin-top: 3rem;
     margin-left: 1rem;
+    @media (max-width: 800px){
+        font-size: 22px;
+}
     `}
 
 
@@ -35,6 +36,11 @@ display: flex;
 width: 50%;
 flex-direction: column;
 background-color: rgba(0,0,0,0.97);
+@media (max-width: 800px){
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+}
 `
 
 export const text = styled.p`
@@ -48,7 +54,12 @@ font-family: ${theme.constants.textTopFontFamily};
 font-size: .8rem;
 color: #bbb;
 line-height: 22px;
-
+@media (max-width: 800px){
+        line-height: 18px;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+}
 `}
 
 `
@@ -61,5 +72,7 @@ background: url(${person});
 background-position: center;
 background-repeat: no-repeat;
 background-size: cover;
-
+@media (max-width: 800px){
+        display: none;
+}
 `
